57dc07257a5b55f76262bd416203cfad2ced39d8
Felipe Valencia <felipe.valencia.2003@gmail.com>
2023-12-27 13:57:27 -0500

    Visualization of operations

--- engine.py
+++ engine.py
@@ -1,7 +1,7 @@
 class Value:
     def __init__(self, data, _children=(), _op='') -> None:
         self.data = data
-        self.prev = set(_children)
+        self._prev = set(_children)
         self._op = _op
 
     def __repr__(self) -> str:
@@ -15,9 +15,36 @@ class Value:
         out = Value(self.data * other.data, (self, other), "*")
         return out
 
+# Visualization
+from graphviz import Digraph
+def trace(root):
+    # build set of nodes and edges
+    nodes, edges = set(), set()
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child, v))
+                build(child)
+    build(root)
+    return nodes, edges
+
+def draw_dot(root):
+    dot = Digraph(format="pdf", graph_attr={"rankdir": "LR"})
+
+    nodes, edges = trace(root)
+    for n in nodes:
+        uid = str(id(n))
+        dot.node(name = uid, label = "{data %.4f}" % (n.data, ), shape="record")
+        if n._op:
+            dot.node(name = uid + n._op, label = n._op)
+            dot.edge(uid+n._op, uid)
+    for n1, n2 in edges:
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+    dot.render("graph", view=True)
 
 a = Value(2.0)
 b = Value(-3.0)
 c = Value(10.0)
 val = a * b + c
-print(val._op)
+draw_dot(val)
