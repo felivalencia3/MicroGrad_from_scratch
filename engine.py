cc4ecdfb1d0094077b0413617a2071c6f5c61819
Felipe Valencia <felipe.valencia.2003@gmail.com>
2023-12-27 15:19:56 -0500

    Calculated Gradients Manually

--- engine.py
+++ engine.py
@@ -1,6 +1,7 @@
 class Value:
     def __init__(self, data, _children=(), _op="", label="") -> None:
         self.data = data
+        self.grad = 0.0
         self._prev = set(_children)
         self._op = _op
         self.label = label
@@ -10,6 +11,10 @@ class Value:
 
     def __add__(self, other):
         out = Value(self.data + other.data, (self, other), "+")
+        return out
+
+    def __sub__(self, other):
+        out = Value(self.data - other.data, (self, other), "-")
         return out
 
     def __mul__(self, other):
@@ -42,14 +47,13 @@ def draw_dot(root):
     nodes, edges = trace(root)
     for n in nodes:
         uid = str(id(n))
-        dot.node(name=uid, label="{%s | data %.4f}" % (n.label, n.data), shape="record")
+        dot.node(name=uid, label="{%s | data %.4f | grad %.4f}" % (n.label, n.data, n.grad), shape="record")
         if n._op:
             dot.node(name=uid + n._op, label=n._op)
             dot.edge(uid + n._op, uid)
     for n1, n2 in edges:
         dot.edge(str(id(n1)), str(id(n2)) + n2._op)
     dot.render("graph", view=True)
-
 
 a = Value(2.0, label="a")
 b = Value(-3.0, label="b")
@@ -58,5 +62,28 @@ e = a * b; e.label = "e"
 d = e + c; d.label = "d"
 f = Value(-2.0, label="f")
 L = d * f; L.label = "L"
-draw_dot(L)
+L.grad = 1.0
+
+def differential():
+    h = 0.0000001
+    a = Value(2.0, label="a")
+    b = Value(-3.0, label="b")
+    c = Value(10.0, label="c")
+    e = a * b; e.label = "e"
+    d = e + c; d.label = "d"
+    f = Value(-2.0, label="f")
+    L = d * f; L.label = "L"
+    L1 = L.data
+
+    a = Value(2.0 + h, label="a")
+    b = Value(-3.0, label="b")
+    c = Value(10.0, label="c")
+    e = a * b; e.label = "e"
+    d = e + c; d.label = "d"
+    f = Value(-2.0, label="f")
+    L = d * f; L.label = "L"
+    L2 = L.data
+    print((L2 - L1) / h)
+
+
 
