259179a6efdec6b2b74c0e7c3bb6c0b1f292511d
Felipe Valencia <felipe.valencia.2003@gmail.com>
2023-12-27 14:02:30 -0500

    Added labels to visual

--- engine.py
+++ engine.py
@@ -1,8 +1,9 @@
 class Value:
-    def __init__(self, data, _children=(), _op='') -> None:
+    def __init__(self, data, _children=(), _op="", label="") -> None:
         self.data = data
         self._prev = set(_children)
         self._op = _op
+        self.label = label
 
     def __repr__(self) -> str:
         return f"Value(data={self.data})"
@@ -15,19 +16,25 @@ class Value:
         out = Value(self.data * other.data, (self, other), "*")
         return out
 
+
 # Visualization
 from graphviz import Digraph
+
+
 def trace(root):
     # build set of nodes and edges
     nodes, edges = set(), set()
+
     def build(v):
         if v not in nodes:
             nodes.add(v)
             for child in v._prev:
                 edges.add((child, v))
                 build(child)
+
     build(root)
     return nodes, edges
+
 
 def draw_dot(root):
     dot = Digraph(format="pdf", graph_attr={"rankdir": "LR"})
@@ -35,16 +42,21 @@ def draw_dot(root):
     nodes, edges = trace(root)
     for n in nodes:
         uid = str(id(n))
-        dot.node(name = uid, label = "{data %.4f}" % (n.data, ), shape="record")
+        dot.node(name=uid, label="{%s | data %.4f}" % (n.label, n.data), shape="record")
         if n._op:
-            dot.node(name = uid + n._op, label = n._op)
-            dot.edge(uid+n._op, uid)
+            dot.node(name=uid + n._op, label=n._op)
+            dot.edge(uid + n._op, uid)
     for n1, n2 in edges:
         dot.edge(str(id(n1)), str(id(n2)) + n2._op)
     dot.render("graph", view=True)
 
-a = Value(2.0)
-b = Value(-3.0)
-c = Value(10.0)
-val = a * b + c
-draw_dot(val)
+
+a = Value(2.0, label="a")
+b = Value(-3.0, label="b")
+c = Value(10.0, label="c")
+e = a * b; e.label = "e"
+d = e + c; d.label = "d"
+f = Value(-2.0, label="f")
+L = d * f; L.label = "L"
+draw_dot(L)
+
