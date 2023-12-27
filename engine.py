6ae800029c437cad09f0de463475418d2d55ab94
Felipe Valencia <felipe.valencia.2003@gmail.com>
2023-12-27 18:35:30 -0500

    Finished Neural Net From Scratch

--- engine.py
+++ engine.py
@@ -14,22 +14,59 @@ class Value:
         return f"Value(data={self.data})"
 
     def __add__(self, other):
+        # Wrap if necessary
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), "+")
 
         def _backward():
-            self.grad = 1.0 * out.grad
-            other.grad = 1.0 * out.grad
+            self.grad += out.grad
+            other.grad += out.grad
 
         out._backward = _backward
         return out
 
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, other):
+        return self + (-other)
 
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), "*")
 
         def _backward():
-            self.grad = other.data * out.grad
-            self.data = self.data * out.grad
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __rmul__(self, other):
+        return self * other
+
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self,), "exp")
+
+        def _backward():
+            self.grad = out.data * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __truediv__(self, other):
+        return self * other**-1
+
+    def __rtruediv__(self, other):
+        return other * self**-1
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float))
+        out = Value(self.data**other, (self, ), f"**{other}")
+        def _backward():
+            self.grad += (other * self.data**(other-1)) * out.grad
 
         out._backward = _backward
         return out
@@ -40,10 +77,29 @@ class Value:
         out = Value(t, (self,), "tanh")
 
         def _backward():
-            self.grad = (1 - t**2) * out.grad
+            self.grad += (1 - t**2) * out.grad
 
         out._backward = _backward
         return out
+
+    def backward(self):
+        ### TOPOLOGICAL SORT
+        topo = []
+        visited = set()
+
+        def build_top(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_top(child)
+                topo.append(v)
+
+        build_top(self)
+
+        # Backpropagate
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
 
 
 # Visualization
@@ -84,48 +140,7 @@ def draw_dot(root):
     dot.render("graph", view=True)
 
 
-a = Value(2.0, label="a")
-b = Value(-3.0, label="b")
-c = Value(10.0, label="c")
-e = a * b
-e.label = "e"
-d = e + c
-d.label = "d"
-f = Value(-2.0, label="f")
-L = d * f
-L.label = "L"
-L.grad = 1.0
-
-
-def differential():
-    h = 0.0000001
-    a = Value(2.0, label="a")
-    b = Value(-3.0, label="b")
-    c = Value(10.0, label="c")
-    e = a * b
-    e.label = "e"
-    d = e + c
-    d.label = "d"
-    f = Value(-2.0, label="f")
-    L = d * f
-    L.label = "L"
-    L1 = L.data
-
-    a = Value(2.0 + h, label="a")
-    b = Value(-3.0, label="b")
-    c = Value(10.0, label="c")
-    e = a * b
-    e.label = "e"
-    d = e + c
-    d.label = "d"
-    f = Value(-2.0, label="f")
-    L = d * f
-    L.label = "L"
-    L2 = L.data
-    print((L2 - L1) / h)
-
-
-def neuron():
+def backpropagate():
     x1 = Value(2.0, label="x1")
     x2 = Value(0.0, label="x2")
     # weights
@@ -141,15 +156,13 @@ def neuron():
     x2w2.label = "x2*w2"
     x1w1x2w2 = x1w1 + x2w2
     x1w1x2w2.label = "x1*w1 + x2*w2"
-    n = x1w1x2w2 + b; n.label = "n"
-    o = n.tanh(); o.label = "o"
-    o.grad = 1.0
-    o._backward() 
-    n._backward()
-
+    n = x1w1x2w2 + b
+    n.label = "n"
+    # o = n.tanh()
+    e = (2*n).exp()
+    o = (e - 1)/ (e + 1)
+    o.label = "o"
+    o.backward()
     draw_dot(o)
 
-def topological_sort():
-    pass
-
-neuron()
+backpropagate()
