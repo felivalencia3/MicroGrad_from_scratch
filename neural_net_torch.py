5c3417e9d16a0040d94e0187be83e018d41b898d
Felipe Valencia <felipe.valencia.2003@gmail.com>
2023-12-27 20:11:24 -0500

    Completed Neural Network - Implemented Gradient Descent

--- neural_net_torch.py
+++ neural_net_torch.py
@@ -9,6 +9,9 @@ class Neuron:
         self.w = [Value(random.uniform(-1, 1)) for _ in range(n_in)]
         self.b = Value(random.uniform(-1, 1))
 
+    def parameters(self):
+        return self.w + [self.b]
+
     def __call__(self, x):
         # w * x (vectors) + b
         act = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
@@ -22,7 +25,11 @@ class Layer:
 
     def __call__(self, x):
         outs = [n(x) for n in self.neurons]
-        return outs[0] if len(outs) == 1 else outs
+        return outs
+
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
+
 
 
 class MultiLayerPerceptron:
@@ -30,15 +37,17 @@ class MultiLayerPerceptron:
         sz = [n_in] + n_outs
         self.layers = [Layer(sz[i], sz[i + 1]) for i in range(len(n_outs))]
 
-    def __call__(self, x):
+    def __call__(self, x) -> list[Value]:
         for layer in self.layers:
             x = layer(x)
         return x
 
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+
 
 x = [2.0, 3.0, -1.0]
 n = MultiLayerPerceptron(3, [4, 4, 1])
-draw_dot(n(x))
 xs = [
     [2.0, 3.0, -1.0],
     [3.0, -1.0, 0.5],
@@ -46,3 +55,26 @@ xs = [
     [1.0, 1.0, -1.0],
 ]
 ys = [1.0, -1.0, -1.0, 1.0]
+
+ypred = []
+# gradient descent
+for i in range(100):
+    # forward pass
+    ypred = [n(x)[0] for x in xs]
+    loss = Value(0)
+    for y_ground_truth, y_out in zip(ys, ypred):
+        loss += (y_out - y_ground_truth) ** 2
+
+    # backward pass + clear grads
+    for p in n.parameters():
+        p.grad = 0.0
+    loss.backward()
+
+    # update
+    for p in n.parameters():
+        # move towards negative gradient
+        p.data += -0.05 * p.grad
+
+    print(i, loss.data)
+
+print(ypred)
